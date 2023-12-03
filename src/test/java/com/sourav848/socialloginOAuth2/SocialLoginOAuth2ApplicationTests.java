98868aa639164d87f2a8fa35f7250127b5b4a9d2
Sourav848 <souravchauhan848@gmail.com>
2023-12-03 23:02:04 +0530

    Initial commit

---
+++ src/test/java/com/sourav848/socialloginOAuth2/SocialLoginOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourav848.socialloginOAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
